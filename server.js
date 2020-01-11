5c3eb2e39dd7e479779357f2554a7b0f0207d8eb
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-10 22:50:45 -0700

    Modify query builder when passing in a range

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ app.get('/', function(req, res){
   let { filters } = req.query;
   filters = JSON.parse(filters)
   let statement = filters.map(function (filter, index) {
-    const { predicate, operator, customValue } = filter;
+    const { predicate, operator, customValue1, customValue2 } = filter;
+    const customFields = customValue2 === null ? customValue1 : `${customValue1} AND ${customValue2}`
     if (index === 0) {
-      return `SELECT * FROM session WHERE ${predicate} ${operator} ${customValue}`
+      return `SELECT * FROM session WHERE ${predicate} ${operator} ${customFields}`
     }
     if (index <= filters.length - 1) {
-      return `AND ${predicate} ${operator} ${customValue}`
+      return `AND ${predicate} ${operator} ${customFields}`
     }
     if (index === filters.length) {
-      return `AND ${predicate} ${operator} ${customValue};`
+      return `AND ${predicate} ${operator} ${customFields};`
     }
   }).join(' ');
   console.log(statement);
